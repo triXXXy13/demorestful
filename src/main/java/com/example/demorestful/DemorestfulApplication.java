ad69c8b47e7ffc57aa7ced56a7c12df77b25be2d
Nemo <hackersaheadddd@gmail.com>
2020-06-24 18:00:56 +0300

    create RESTful project

---
+++ src/main/java/com/example/demorestful/DemorestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemorestfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demorestful/DemorestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemorestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
