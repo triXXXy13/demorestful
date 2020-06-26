aed7cb53a0d7a2caf8b5dac61cbb29c530214dce
Nemo <hackersaheadddd@gmail.com>
2020-06-26 14:52:25 +0300

    add RestController

--- src/main/java/com/example/demorestful/controller/MainController.java
+++ src/main/java/com/example/demorestful/controller/MainController.java
@@ -3,10 +3,12 @@ package com.example.demorestful.controller;
 
 import com.example.demorestful.entity.Task;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class MainController {
 
     @GetMapping("/")
